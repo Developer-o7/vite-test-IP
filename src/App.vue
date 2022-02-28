b70fa9c513cd901a0251c69cab2cdd5e73605691
Muzaffar <muzaffarmasordinov@gmail.com>
2022-02-28 12:16:28 +0500

    Created 'button'

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-<div class="container card">
-  <label for="text">Enter your City</label>
-  <input type="text">
-</div>
+  <div class="container card">
+    <label for="text">Enter your City</label>
+    <div class="card flex">
+      <input type="text" />
+      <button type="button" class="bg-indigo-500 ..." disabled>
+        <svg class="motion-reduce:hidden animate-spin ..." viewBox="0 0 24 24"></svg>
+        Processing...
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
